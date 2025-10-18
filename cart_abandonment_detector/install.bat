6d7498f565700c5b193295def7eee483a50a65e1
ChanithW <127946094+ChanithW@users.noreply.github.com>
2025-10-18 15:34:12 +0530

    Remove contents from install.bat script

--- cart_abandonment_detector/install.bat
+++ cart_abandonment_detector/install.bat
@@ -1,9 +1,1 @@
-@echo off
-REM Cart Abandonment Detector - Windows Installation Script
-echo ============================================================
-echo    Cart Abandonment Detector - Installation
-echo ============================================================
-echo.
 
-
-pause
