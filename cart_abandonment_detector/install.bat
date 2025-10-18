bcc9b95272272cd4b3f7af1aa566ea923323e91d
ChanithW <127946094+ChanithW@users.noreply.github.com>
2025-10-18 15:31:08 +0530

    Remove Python dependency installation from install.bat

--- cart_abandonment_detector/install.bat
+++ cart_abandonment_detector/install.bat
@@ -5,13 +5,5 @@ echo    Cart Abandonment Detector - Installation
 echo ============================================================
 echo.
 
-echo Installing Python dependencies...
-echo.
-
-pip install google-generativeai
-pip install scikit-learn
-pip install numpy
-pip install flask-mail
-
 
 pause
